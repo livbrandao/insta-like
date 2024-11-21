829eb27302cc6107f56e107f4f58bcff4730c729
Livia Brandao <liviatoledo27@gmail.com>
2024-11-21 20:16:20 -0300

    implementing image storage and uploads

--- src/models/postsModel.js
+++ src/models/postsModel.js
@@ -2,8 +2,14 @@ import conectarAoBanco from "../config/dbConfig.js";
 
 const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
 
-export default async function getPosts() {
+export async function getPosts() {
   const db = await conexao.db("imersao-instalike");
   const colecao = db.collection("posts");
   return colecao.find().toArray();
+}
+
+export async function criarPost(novoPost) {
+  const db = await conexao.db("imersao-instalike");
+  const colecao = db.collection("posts");
+  return colecao.insertOne(novoPost);
 }
